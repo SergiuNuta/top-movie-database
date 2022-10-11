5b3f8de3b3647839562add75b720ba67d11b7e9b
serghei <serghei@murphyvarley.com>
2022-10-11 19:13:14 +0100

    add vuex and tailwind

---
+++ src/components/MovieList.vue
@@ -0,0 +1,21 @@
+<template>
+<h1>Made By Actions</h1>
+  <div v-for='movie in movies.items' :key='movie.id'>
+    {{movie.id}} {{movie.title}} {{movie.year}}
+    </div>
+</template>
+
+<script setup>
+import { onMounted, computed } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+
+const movies = computed(() => {
+  return store.state.movies
+})
+
+onMounted(() => {
+  store.dispatch('fetchMovies')
+})
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,34 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    movies: []
+  },
+  getters: {
+    getMovies: (state) => state.movies
   },
   mutations: {
+    SET_Movies (state, movies) {
+      state.movies = movies
+    }
   },
   actions: {
-  },
-  modules: {
+    async fetchMovies ({ commit }) {
+      try {
+        const data = await axios.get('https://imdb-api.com/en/API/Top250Movies/k_6fg5u00c')
+        commit('SET_Movies', data.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    // async searchMovie ({ commit }) {
+    //   try {
+    //     const data = await axios.get(`https://imdb-api.com/en/API/SearchMovie/k_6fg5u00c/${title}`)
+    //     commit('SET_Movie', data.data)
+    //   } catch (error) {
+    //     console.log(error)
+    //   }
+    // }
   }
 })
