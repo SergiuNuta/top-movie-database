1aa12ae9ff99271ec83d405f19f9ff701bf2c96c
serghei <serghei@murphyvarley.com>
2022-10-12 15:43:46 +0100

    fix image source

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -3,7 +3,7 @@
                 <div className="w-60 min-h-full mb-5">
                     <div className="w-full flex flex-col justify-between shadow hover:shadow-lg transition duration-300 ease-in-out cursor-pointer group">
                         <div className="overflow-hidden relative w-full">
-                            <img className="w-full h-80 object-cover transition duration-700 ease-in-out group-hover:opacity-60" :src='movie.image' :alt='movie.title' />
+                            <img className="w-full h-80 object-fill transition duration-700 ease-in-out group-hover:opacity-60" :src='url(movie.image)' :alt='movie.title' />
                         </div>
                         <div className="px-4 py-3">
                             <a href="#" className=""><h1 className="text-gray-800 font-semibold text-lg hover:text-red-500 transition duration-300 ease-in-out">{{ movie.fullTitle }}</h1></a>
@@ -21,4 +21,14 @@
 defineProps({
   movie: {}
 })
+
+const url = (data) => {
+  const occurence = data.split('@').length - 1
+  const urlCopy = data.split('@')
+  if (occurence === 2) {
+    return urlCopy[0] + '@@' + '.jpg'
+  } else {
+    return urlCopy[0] + '@' + '.jpg'
+  }
+}
 </script>
