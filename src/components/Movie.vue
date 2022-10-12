32653b6240295ced66081daa9f5b364bc5f6bbaa
serghei <serghei@murphyvarley.com>
2022-10-12 20:18:54 +0100

    add star for imdb rating

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,16 +1,17 @@
 <template>
-    <div calssName="movieCard">
+    <div calssName="movieCard h-48">
                 <div className="w-60 min-h-full mb-5">
-                    <div className="w-full flex flex-col justify-between shadow hover:shadow-lg transition duration-300 ease-in-out cursor-pointer group">
-                        <div className="overflow-hidden relative w-full">
+                    <div className="relative w-full flex flex-col justify-between shadow hover:shadow-lg transition duration-300 ease-in-out cursor-pointer group">
+                        <div className="overflow-hidden w-full">
                             <img className="w-full h-80 object-fill transition duration-700 ease-in-out group-hover:opacity-60" :src='url(movie.image)' :alt='movie.title' />
                         </div>
-                        <div className="px-4 py-3">
-                            <a href="#" className=""><h1 className="text-gray-800 font-semibold text-lg hover:text-red-500 transition duration-300 ease-in-out">{{ movie.fullTitle }}</h1></a>
-                        <div className="flex py-2">
-                            <p className="mr-2 text-xs text-gray-600">{{ movie.imDbRating }}</p>
+                        <div className="pl-2 py-3 h-28">
+                            <a href="#" className=""><h1 className="text-gray-800 font-semibold text-base hover:text-red-500 transition duration-300 ease-in-out">{{ movie.fullTitle }}</h1></a>
                         </div>
-                    </div>
+                        <div className="pt-2 pl-2 absolute bottom-0 flex justify-items-center items-baseline">
+                                <img className="h-5" src="../assets/star-icon.png" alt="star">
+                                <p className="mr-2 text-2xl  text-gray-600">{{ movie.imDbRating }}</p>
+                            </div>
                     </div>
                 </div>
     </div>
